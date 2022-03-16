b8d3181c72001ab108ed836f56d5f6ffe95cc919
pieroolivares <piero-olivares@hotmail.com>
2022-03-16 19:00:08 +0100

    added experience section and css queries for devices

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 import './about.css';
+import SALCHAS from "../../assets/salchas.png";
+import { FaAward } from "react-icons/fa";
+import { HiAcademicCap } from "react-icons/hi";
+import { FaFolderOpen } from "react-icons/fa"
+
 
 const About = () => {
   return (
-    <section id="about">About
+    <section id="about">
+    <h5>Get to Know</h5>
+    <h2>About me</h2>
+
+    <div className="container about__container">
+      <div className="about__me">
+        <div className="about__me-image">
+          <img src={SALCHAS} alt="About Image"/>
+        </div>
+      </div>
+
+      <div className="about__content">
+        <div className="about__cards">
+          <article className="about__card">
+            <FaAward className="about__icon"/>
+            <h5>Experience</h5>
+            <small>estudios</small>
+          </article>
+
+          <article className="about__card">
+            <HiAcademicCap className="about__icon"/>
+            <h5>Academy</h5>
+            <small>estudios</small>
+          </article>
+
+          <article className="about__card">
+            <FaFolderOpen className="about__icon"/>
+            <h5>Projects</h5>
+            <small>estudios</small>
+          </article>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima dolor magni dignissimos iste accusantium, placeat voluptas mollitia. In blanditiis magni magnam architecto non aut, ratione dignissimos, tenetur optio, quia explicabo!
+        </p>
+
+        <a href="#contact" className="btn btn-primary">Let's Talk</a>
+      </div>
+    </div>
     </section>
   )
 }
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BsLinkedin } from "react-icons/bs"
-import { FaGithubSquare } from "react-icons/fa"
-import { AiOutlineSlackSquare } from "react-icons/ai"
+import { BsGithub } from "react-icons/bs"
+import { BsSlack } from "react-icons/bs"
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
       <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-      <a href="https://github.com" target="_blank"><FaGithubSquare/></a>
-      <a href="https://linkedin.com" target="_blank"><AiOutlineSlackSquare /></a>
+      <a href="https://github.com" target="_blank"><BsGithub/></a>
+      <a href="https://linkedin.com" target="_blank"><BsSlack /></a>
     </div>
   )
 }
